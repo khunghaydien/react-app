99412f86851fbfed05310708b3539e6e1c716cfe
MH_DatPH <datph@mor.com.vn>
2023-02-02 16:59:53 +0700

    create react-app

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  const Root = "App"
+  return (
+    <div className={`${Root}`}>
+      <div className={`${Root}-header`}>
+      </div>
+    </div>
+  );
+}
+
+export default App;
